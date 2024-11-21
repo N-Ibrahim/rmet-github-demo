519d6f88aad52b621fed55da7261101b6931e7a6
NIbrahim <nasirpolo@yahoo.com>
2024-11-21 12:46:53 +0000

    Update MyProgram.py

--- MyProgram.py
+++ MyProgram.py
@@ -5,9 +5,6 @@ def add(first_number, second_number):
 def subtract(first_number, second_number):
     return first_number - second_number
 
-def multiply(first_number, second_number):
-    return first_number * second_number
-
 def divide(first_number, second_number):
     return first_number / second_number
     print(divide)
@@ -15,3 +12,5 @@ def divide(first_number, second_number):
 print(add(-5,7))
 print(subtract(-5,7))
 print(multiply(-5,7))
+
+print("New Result")
