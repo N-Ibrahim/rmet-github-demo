7eec3de871b43c951ffc42db920dd8b3b1790fa1
N-Ibrahim <52959966+N-Ibrahim@users.noreply.github.com>
2024-11-21 13:02:23 +0000

    Update MyProgram.py

--- MyProgram.py
+++ MyProgram.py
@@ -7,7 +7,8 @@ def subtract(first_number, second_number):
 
 def divide(first_number, second_number):
     return first_number / second_number
-    print(divide)
+
+print("divide")
 
 print(add(-5,7))
 print(subtract(-5,7))
